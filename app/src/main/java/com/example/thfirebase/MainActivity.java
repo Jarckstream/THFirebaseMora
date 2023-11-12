1ef3036a55be4a21aa0c0e9ec3086d28155eb70b
Jarckstream <nicomora32@gmail.com>
2023-11-12 04:31:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/thfirebase/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.thfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseReference mydb;
+    TextView temperatura, humedad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("FirebaseData","onCreate called");
+        temperatura = findViewById(R.id.temp);
+        humedad = findViewById(R.id.humed);
+        mydb = FirebaseDatabase.getInstance().getReference().child("Sensor");
+        try{
+            mydb.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Log.d("FirebaseData","onCreate called");
+                    if ( temperatura == null || humedad == null){
+                        Log.e("FirebaseData","TextViews not initialized");
+                        return;
+                    }
+                    if(dataSnapshot.exists()){
+                        if(dataSnapshot.child("temp").exists() && dataSnapshot.child("hum").exists()){
+                            String tempdata = dataSnapshot.child("temp").getValue(String.class);
+                            String humdata = dataSnapshot.child("hum").getValue(String.class);
+                            if(tempdata != null && humdata != null){
+                                temperatura.setText(tempdata);
+                                humedad.setText(humdata);
+                            }else{
+
+                            }
+                        }else{
+
+                        }
+                    }else{
+                        Log.d("FirebaseData","DataSnapshot does not exists");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("FirebaseData", "onCancelled" + error.getMessage());
+
+                }
+            });
+        }catch (Exception e){
+            Log.e("FirebaseData","Error" + e.getMessage());
+        }
+    }
+}
